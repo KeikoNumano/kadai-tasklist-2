729c6e6fd19c6eb9caaf094fd5051eeee9c1c358
keiko.numano <keiko.n816211@gmail.com>
2020-07-12 13:03:06 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user, only: [:destroy]
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.all
   end
 
   def show
@@ -12,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'task が正常に投稿されました'
@@ -28,7 +30,7 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'task は正常に更新されました'
@@ -40,7 +42,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'task は正常に削除されました'
@@ -52,5 +53,12 @@ class TasksController < ApplicationController
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
